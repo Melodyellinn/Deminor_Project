e090af56382be6444d330ccaa62379eb29ab5e34
Melody <welodya@gmail.com>
2023-06-20 15:09:28 +0200

    exercice2 question1

---
+++ main.py
@@ -0,0 +1,11 @@
+# ----------- TP Démineur ----------- #
+
+# --- Programme de base --- #
+import sys
+
+file_name = sys.argv[0]
+hauteur = sys.argv[1]
+largeur = sys.argv[2]
+print("Document", sys.argv)
+print("Hauteur :", hauteur)
+print("Largeur :", largeur)

db4d2d50e8cfe2cbae556690c4ec6f6f3d58fc1d
Melody <welodya@gmail.com>
2023-06-20 15:59:11 +0200

    exercice2 question3 faire un programme avec des inputs

--- main.py
+++ main.py
@@ -15,7 +15,21 @@
 
 # ---- 2 ---- #
 
-while True:
-    x = input("Entrez x :")
-    y = input("Entrez y :")
+# while True:
+#     x = input("Entrez x :")
+#     y = input("Entrez y :")
 
+# ---- 3 ---- #
+while True:
+    input_player = input("Entrez deux chiffres (exp : 5 2) :")
+
+    if input_player[0] == "F":
+        input_player_split = input_player.split(" ")
+        x = input_player_split[1]
+        y = input_player_split[2]
+        print(f"Flagger la case {x}, {y}")
+    else:
+        xy_split = input_player.split(" ")
+        x = xy_split[0]
+        y = xy_split[1]
+        print(f"Ouvrir la case {x}, {y}")

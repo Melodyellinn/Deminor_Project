723c08759531b3a3a5faa88c7252d4c2717438f7
Melody <welodya@gmail.com>
2023-06-21 14:26:10 +0200

    exercice2 question6: ajout des commandes newgame et quit

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ class MineSweeper:
     def __init__(self):
         self.is_playing = False
 
+    def new_game(self, grid_size):
+        self.is_playing = True
+        print(f'La partie commence avec une grille de : {grid_size}')
+
     def open(self, x, y):
         if not self.is_playing:
             raise NotRunningError("Pas de partie en cours")
@@ -20,30 +24,43 @@ class MineSweeper:
             raise NotRunningError("Pas de partie en cours")
         print(f"Flagger la case {x}, {y}")
 
-    def new_game(self, grid_size):
-        self.is_playing = True
-        print(f'La partie commence avec une grille de : {grid_size}')
-
 
 ms = MineSweeper()
-hauteur = int(input("Entrez la hauteur de la grille : "))
-largeur = int(input("Entrez la largeur de la grille : "))
-grid = hauteur * largeur
+
+hauteur = ""
+largeur = ""
+
+while hauteur == "":
+    hauteur = input("Entrez la hauteur de la grille : ")
+    print('Veuillez indiquer un chiffre')
+
+while largeur == "":
+    largeur = input("Entrez la largeur de la grille : ")
+    print('Veuillez indiquer un chiffre')
+
+grid = int(hauteur) * int(largeur)
 ms.new_game(grid)
 
 while True:
-    input_player = input("Entrez deux chiffres (5 2 ou F 1 3): ")
+    input_player = input("Entrez deux chiffres (5 2 ou F 1 3), newgame ou quit : ")
     if not input_player:
         print('Indique quelque chose !')
-    input_player_split = input_player.split(" ")
 
     if input_player:
-        if input_player[0] == "F":
-            haut = input_player_split[1]
-            larg = input_player_split[2]
-            ms.flag(haut, larg)
+        if input_player == "newgame":
+            ms.new_game(grid)
+        elif input_player == "quit":
+            print('Fin de partie')
+            break
         else:
-            haut = input_player_split[0]
-            larg = input_player_split[1]
-            ms.open(haut, larg)
+            input_player_split = input_player.split(" ")
+
+            if input_player[0] == "F":
+                haut = input_player_split[1]
+                larg = input_player_split[2]
+                ms.flag(haut, larg)
+            else:
+                haut = input_player_split[0]
+                larg = input_player_split[1]
+                ms.open(haut, larg)
 

87d8a6f4bff0f5a6d370a4e353ddb5168e03672c
Melody <welodya@gmail.com>
2023-06-22 11:27:02 +0200

    FIXE de la question 6

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # ----------- TP Démineur ----------- #
-
+import re
 # --- Programme de base --- #
 import sys
 from abc import ABC, abstractmethod
@@ -13,10 +13,10 @@ class MineSweeper:
     def __init__(self):
         self.is_playing = False
 
-    def new_game(self, grid_size):
+    def new_game(self, height, width):
         self.is_playing = True
-        print(f'La partie commence avec une grille de : {grid_size}')
-        new_grid = Grid()
+        print(f'La partie commence avec une grille de : {width} et {height}')
+        new_grid = Grid(int(width), int(height))
 
     def open(self, x, y):
         if not self.is_playing:
@@ -81,9 +81,9 @@ hauteur = sys.argv[1]
 largeur = sys.argv[2]
 print("Hauteur de la grille:", hauteur)
 print("Largeur de la grille :", largeur)
-grid = int(hauteur) * int(largeur)
+# grid = int(hauteur) * int(largeur)
 
-ms.new_game(grid)
+ms.new_game(hauteur, largeur)
 
 while True:
     input_player = input("Entrez deux chiffres (5 2 ou F 1 3), newgame ou quit : ")
@@ -91,31 +91,31 @@ while True:
         print('Indique quelque chose !')
 
     if input_player:
-        input_player_split = input_player.split(" ")
-
-        if input_player == "newgame":
-            ms.new_game(grid)
-        elif input_player_split[0] == "newgame" and len(input_player_split) > 1:
-            print('test')
-            grid = int(input_player_split[1]) * int(input_player_split[2])
-            ms.new_game(grid)
-        elif input_player == "quit":
-            print('Fin de partie')
-            break
-        else:
+        if re.match(r'\w', input_player):
             input_player_split = input_player.split(" ")
 
-            if input_player[0] == "F":
-                haut = input_player_split[1]
-                larg = input_player_split[2]
-                try:
-                    ms.flag(haut, larg)
-                except NotRunningError:
-                    print("La partie n'est pas en cours")
+            if input_player == "newgame":
+                ms.new_game(hauteur, largeur)
+            elif input_player_split[0] == "newgame" and len(input_player_split) > 1:
+                print('test')
+                ms.new_game(input_player_split[1], input_player_split[2])
+            elif input_player == "quit":
+                print('Fin de partie')
+                break
             else:
-                haut = input_player_split[0]
-                larg = input_player_split[1]
-                try:
-                    ms.open(haut, larg)
-                except NotRunningError:
-                    print("La partie n'est pas en cours")
+                input_player_split = input_player.split(" ")
+
+                if input_player[0] == "F":
+                    haut = input_player_split[1]
+                    larg = input_player_split[2]
+                    try:
+                        ms.flag(haut, larg)
+                    except NotRunningError:
+                        print("La partie n'est pas en cours")
+                else:
+                    haut = input_player_split[0]
+                    larg = input_player_split[1]
+                    try:
+                        ms.open(haut, larg)
+                    except NotRunningError:
+                        print("La partie n'est pas en cours")
